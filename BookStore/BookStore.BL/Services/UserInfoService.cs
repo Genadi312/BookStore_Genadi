50e609b1c4c2d776e74026253e79879bcac56e60
Genadi Chakarov <genadichakarov@gmail.com>
2023-05-09 22:03:13 +0300

    Tests fixed and Autorization started (Repo, Service,)

---
+++ BookStore/BookStore.BL/Services/UserInfoService.cs
@@ -0,0 +1,24 @@
+﻿using BookStore.BL.Interfaces;
+using BookStore.DL.Interfaces;
+using BookStore.Models.Models;
+
+namespace BookStore.BL.Services
+{
+    public class UserInfoService : IUserInfoService
+    {
+        private readonly IUserInfoRepository _userInfoRepository;
+
+        public UserInfoService(IUserInfoRepository userInfoRepository)
+        {
+            _userInfoRepository = userInfoRepository;
+        }
+
+
+        public Task<UserInfo?> GetUserInfoAsync(string userName, string password)
+        {
+            return _userInfoRepository.GetUserInfoAsync(userName, password);
+        }
+
+
+    }
+}
---
+++ BookStore/BookStore.DL/Interfaces/IUserInfoRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Models.Models;
+
+namespace BookStore.DL.Interfaces
+{
+    public interface IUserInfoRepository
+    {
+        public Task<UserInfo?> GetUserInfoAsync(string userName, string password);
+    }
+}
